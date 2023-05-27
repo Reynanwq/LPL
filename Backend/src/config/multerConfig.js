662e39c2e7cb74fb120736e0344a93781f1ffec5
Reynanwq <reynanwq@gmail.com>
2023-05-27 00:35:01 -0300

    verificando se o arquivo e jpg ou png

--- Backend/src/config/multerConfig.js
+++ Backend/src/config/multerConfig.js
@@ -4,12 +4,19 @@ import { extname, resolve } from 'path';
 const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
-    Storage: multer.diskStorage({
-        destination(req, file, cb) => {
-            cb(null, resolve(_dirname, '..', '..', 'uploads'));
+    //verifica se é PNG OU JPG
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype != 'image/png' && file.mimetype != 'image/jpg') {
+            return cb(new multer.MulterError('Arquivo precisa ser PNG ou JPG'));
+        }
+        return cb(null, true);
+    },
+    storage: multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, resolve(__dirname, '..', '..', 'uploads'));
         },
         filename: (req, file, cb) => {
             cb(null, `${Date.now()}_${aleatorio()}${extname(file.originalname)}`);
         }
-    }),
+    })
 };
